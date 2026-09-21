02e48babbf5f203fb3984d3cacbafa012a34d06f
Mohamed <mohamedmohamed1812@gmail.com>
2026-09-21 21:22:58 +0100

    Claude: removed unused response properties

--- aichat/Models/ChatCompletionResponse.cs
+++ aichat/Models/ChatCompletionResponse.cs
@@ -4,38 +4,17 @@ namespace aichat.Models
 
     public class ChatCompletionResponse
     {
-        [JsonPropertyName("id")]
-        public string Id { get; set; } = string.Empty;
-
-        [JsonPropertyName("object")]
-        public string Object { get; set; } = string.Empty;
-
-        [JsonPropertyName("created")]
-        public long Created { get; set; }
-
-        [JsonPropertyName("model")]
-        public string Model { get; set; } = string.Empty;
-
         [JsonPropertyName("choices")]
         public List<ChatCompletionChoice> Choices { get; set; } = new();
 
         [JsonPropertyName("usage")]
         public ChatCompletionUsage? Usage { get; set; }
-
-        [JsonPropertyName("system_fingerprint")]
-        public string? SystemFingerprint { get; set; }
     }
 
     public class ChatCompletionChoice
     {
-        [JsonPropertyName("index")]
-        public int Index { get; set; }
-
         [JsonPropertyName("message")]
         public ChatCompletionMessage Message { get; set; } = new();
-
-        [JsonPropertyName("finish_reason")]
-        public string? FinishReason { get; set; }
     }
 
     public class ChatCompletionMessage
